cfa9246e2c22d52607d4199e7bad23a6ff481e7b
Ramachandra <ramachandra.hegde@weinvest.net>
2024-05-13 23:44:58 +0530

    Adding multiple numbers in string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,11 @@
 class StringCalculator
   def self.add(numbers)
+    # Return 0 for empty string
     return 0 if numbers.empty?
+
+    # Split numbers based on default delimiters
+    num_list = numbers.strip.split(/\n|,/)
+    # Convert strings to integers and sum them
+    num_list.sum(&:to_i)
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,5 +5,17 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an empty string' do
       expect(StringCalculator.add('')).to eq(0)
     end
+
+    it 'returns the number for a single number' do
+      expect(StringCalculator.add('1')).to eq(1)
+    end
+
+    it 'returns the sum of two numbers' do
+      expect(StringCalculator.add('1,5')).to eq(6)
+    end
+
+    it 'allows new lines between numbers' do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
   end
 end
